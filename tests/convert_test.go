cb70baa668cda201134122eef5b89f28bcb66038
koalatea <koalateac@gmail.com>
2019-08-27 20:31:31 -0500

    first failing test set up for basic testing

--- tests/convert_test.go
+++ tests/convert_test.go
@@ -1,1 +1,27 @@
 package tests
+
+import (
+	"io"
+	"testing"
+
+	"github.com/kcarretto/paragon/interpreter"
+)
+
+func convertTestFunc(argParse interpreter.ArgParser, output io.Writer) (interpreter.Retval, error) {
+	return nil, nil
+}
+
+func TestConvert(t *testing.T) {
+	newFunc := interpreter.Func(convertTestFunc)
+	i := interpreter.New()
+	l := interpreter.Library{"my_func": newFunc}
+	i.AddLibrary("mylib", l)
+
+	script := interpreter.NewScript("myscript", []byte(myscript))
+	output := &Output{}
+	err := i.Execute(script, output)
+	if err != nil {
+		t.Error("Error executing test: ", err)
+	}
+	t.Error("no test written")
+}
