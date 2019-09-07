33dc22e1372bcdc65af62c6388a838d471821655
koalatea <koalateac@gmail.com>
2019-09-07 18:18:31 -0500

    fixed the issues using reflect for convert

--- interpreter/convert.go
+++ interpreter/convert.go
@@ -1,6 +1,10 @@
 package interpreter
 
-import "go.starlark.net/starlark"
+import (
+	"reflect"
+
+	"go.starlark.net/starlark"
+)
 
 func convertToStarlark(value interface{}) (starlark.Value, error) {
 	if value == nil {
@@ -23,35 +27,68 @@ func convertToStarlark(value interface{}) (starlark.Value, error) {
 		return starlark.Float(v), nil
 	case string:
 		return starlark.String(v), nil
-	case []interface{}:
-		var elems []starlark.Value
-		for _, elem := range v {
-			val, err := convertToStarlark(elem)
-			if err != nil {
-				return nil, err
+	// case []interface{}:
+	// 	var elems []starlark.Value
+	// 	for _, elem := range v {
+	// 		val, err := convertToStarlark(elem)
+	// 		if err != nil {
+	// 			return nil, err
+	// 		}
+	// 		elems = append(elems, val)
+	// 	}
+	// 	return starlark.NewList(elems), nil
+	// case map[interface{}]interface{}:
+	// 	dict := starlark.NewDict(len(v))
+
+	// 	for index, elem := range v {
+	// 		key, err := convertToStarlark(index)
+	// 		if err != nil {
+	// 			return nil, err
+	// 		}
+
+	// 		val, err := convertToStarlark(elem)
+	// 		if err != nil {
+	// 			return nil, err
+	// 		}
+
+	// 		if err = dict.SetKey(key, val); err != nil {
+	// 			return nil, err
+	// 		}
+	// 	}
+	// 	return dict, nil
+	default:
+		reflectV := reflect.ValueOf(value)
+		switch reflectV.Kind() {
+		case reflect.Slice:
+			var elems []starlark.Value
+			for i := 0; i < reflectV.Len(); i++ {
+				val, err := convertToStarlark(reflectV.Index(i).Interface())
+				if err != nil {
+					return nil, err
+				}
+				elems = append(elems, val)
 			}
-			elems = append(elems, val)
+			return starlark.NewList(elems), nil
+		case reflect.Map:
+			dict := starlark.NewDict(len(reflectV.MapKeys()))
+
+			iter := reflectV.MapRange()
+			for iter.Next() {
+				key, err := convertToStarlark(iter.Key().Interface())
+				if err != nil {
+					return nil, err
+				}
+
+				val, err := convertToStarlark(iter.Value().Interface())
+				if err != nil {
+					return nil, err
+				}
+				if err = dict.SetKey(key, val); err != nil {
+					return nil, err
+				}
+			}
+			return dict, nil
 		}
-		return starlark.NewList(elems), nil
-	case map[interface{}]interface{}:
-		dict := starlark.NewDict(len(v))
-
-		for index, elem := range v {
-			key, err := convertToStarlark(index)
-			if err != nil {
-				return nil, err
-			}
-
-			val, err := convertToStarlark(elem)
-			if err != nil {
-				return nil, err
-			}
-
-			if err = dict.SetKey(key, val); err != nil {
-				return nil, err
-			}
-		}
-		return dict, nil
 	}
 	return nil, nil
 }
