9fd3f4498ca534390a2c1935cd8ce818ccbca6c6
koalatea <koalateac@gmail.com>
2019-09-07 18:29:51 -0500

    removed comments left behind from old code testing

--- interpreter/convert.go
+++ interpreter/convert.go
@@ -27,35 +27,6 @@ func convertToStarlark(value interface{}) (starlark.Value, error) {
 		return starlark.Float(v), nil
 	case string:
 		return starlark.String(v), nil
-	// case []interface{}:
-	// 	var elems []starlark.Value
-	// 	for _, elem := range v {
-	// 		val, err := convertToStarlark(elem)
-	// 		if err != nil {
-	// 			return nil, err
-	// 		}
-	// 		elems = append(elems, val)
-	// 	}
-	// 	return starlark.NewList(elems), nil
-	// case map[interface{}]interface{}:
-	// 	dict := starlark.NewDict(len(v))
-
-	// 	for index, elem := range v {
-	// 		key, err := convertToStarlark(index)
-	// 		if err != nil {
-	// 			return nil, err
-	// 		}
-
-	// 		val, err := convertToStarlark(elem)
-	// 		if err != nil {
-	// 			return nil, err
-	// 		}
-
-	// 		if err = dict.SetKey(key, val); err != nil {
-	// 			return nil, err
-	// 		}
-	// 	}
-	// 	return dict, nil
 	default:
 		reflectV := reflect.ValueOf(value)
 		switch reflectV.Kind() {
