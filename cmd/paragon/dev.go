091b0e0f24ad03acc68c83dceed4be246a0c7320
Kyle Carretto <kcarretto@gmail.com>
2019-09-11 23:35:30 -0700

    Updated cmd with dev and prod build options

---
+++ cmd/paragon/dev.go
@@ -0,0 +1,31 @@
+// +build development
+
+package main
+
+import (
+	"github.com/kcarretto/paragon/agent"
+	"go.uber.org/zap"
+)
+
+func getLogger() *zap.Logger {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	return logger
+}
+
+func Run(logger *zap.Logger) {
+	agent := agent.New(
+		logger,
+		Runner("Stuff"),
+	)
+
+	agent.Run()
+	defer func() {
+		if err := agent.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+}
--- cmd/paragon/main.go
+++ cmd/paragon/main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/kcarretto/paragon/agent"
+	"go.uber.org/zap"
 )
 
+type Runner string
+
+func (runner Runner) Run(context.Context, *zap.Logger, agent.Task) {}
+
 func main() {
-	fmt.Println("Paragon stub")
+	interupts := make(chan os.Signal, 1)
+	signal.Notify(interupts, syscall.SIGINT, syscall.SIGTERM)
+
+	logger := getLogger()
+
+	go Run(logger)
+
+	switch <-interupts {
+	case syscall.SIGINT:
+		logger.Info("Received interupt signal")
+	case syscall.SIGTERM:
+		logger.Error("Received terminate signal")
+	}
+
 }
