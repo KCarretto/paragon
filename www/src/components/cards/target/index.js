d065c70001d4f0c578935b047725b25138c7174d
Kyle Carretto <kcarretto@gmail.com>
2019-12-10 21:16:27 -0800

    Restructure react components

--- www/src/components/cards/target/index.js
+++
@@ -1,2 +0,0 @@
-export { default as XTargetCard } from './XTargetCard';
-export { default as XTargetCardGroup } from './XTargetCardGroup';
--- www/src/models/target.jsx
+++
@@ -1,5 +0,0 @@
-// @flow
-
-type Target = {
-    id: Number,
-}
