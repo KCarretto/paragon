093de6e7c41a5d56bda8da934b8bd96317f1ea8a
Kyle Carretto <kcarretto@gmail.com>
2019-12-10 21:16:27 -0800

    Restructure react components

--- www/src/views/index.js
+++ www/src/views/index.js
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 export { default as XMultiJobView } from './XMultiJobView';
 export { default as XMultiTargetView } from './XMultiTargetView';
 export { default as XTargetView } from './XTargetView';
 export { default as XTaskView } from './XTaskView';
+=======
+export { default as XMultiTargetView } from './XMultiTargetView';
+export { default as XTargetView } from './XTargetView';
+>>>>>>> Restructure react components
 
