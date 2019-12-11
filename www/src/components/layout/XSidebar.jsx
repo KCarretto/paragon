06b5d6073c73c66194e999b8e1b96502fd96e203
Kyle Carretto <kcarretto@gmail.com>
2019-12-10 22:39:17 -0800

    Added bug report sidemenu option

--- www/src/components/layout/XSidebar.jsx
+++ www/src/components/layout/XSidebar.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Menu, Sidebar } from 'semantic-ui-react';
+import { Icon, Menu, Sidebar } from 'semantic-ui-react';
 import './index.css';
 
 const XSidebar = (props) => (
@@ -20,6 +20,7 @@ const XSidebar = (props) => (
             {props.routeMap ? props.routeMap.map((value, index) => {
                 return <Menu.Item as={Link} to={value.link} key={index}>{value.icon}{value.title}</Menu.Item>
             }) : []}
+            <Menu.Item href="https://github.com/kcarretto/paragon/issues/new"><Icon name='bug' />Bug</Menu.Item>
         </Sidebar>
         <Sidebar.Pusher className='XContent'>
             {props.children}
