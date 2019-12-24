81df1ebb8ea1684e20ae0231538a1c216d3329db
Kyle Carretto <kcarretto@gmail.com>
2019-12-23 17:13:17 -0800

    GraphQL Connection

--- www/src/views/XTaskView.jsx
+++ www/src/views/XTaskView.jsx
@@ -1,11 +1,32 @@
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { Loader } from 'semantic-ui-react';
+
+const TASK_QUERY = gql`
+    query Task($id: ID!) {
+    task(id: $id) {
+        id
+        output
+    }
+  }
+`;
 
 const XTaskView = () => {
     let { id } = useParams();
 
+    const { loading, error, data } = useQuery(TASK_QUERY, {
+        variables: { id },
+    });
+    if (loading) return (<Loader active />);
+    if (error) return (`${error}`);
+
     return (
-        <h1>TASK: {id}</h1>
+        <div>
+            <h1>TASK: {id}</h1>
+            <h2>{data}</h2>
+        </div>
     );
 }
 
