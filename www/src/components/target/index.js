92a5b269f3e6c463faad0a3511671a5a37ce68da
Kyle Carretto <kcarretto@gmail.com>
2020-01-04 16:53:20 -0800

    Added target create form and modal

--- www/src/components/target/index.js
+++ www/src/components/target/index.js
@@ -1,2 +1,4 @@
 export { default as XTargetCard } from './XTargetCard';
+export { default as XTargetCreateForm } from './XTargetCreateForm';
+export { default as XTargetCreateModal } from './XTargetCreateModal';
 
