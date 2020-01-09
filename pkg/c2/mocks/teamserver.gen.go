70d1bc1031a61daa9d076c716f7dfa814a316e64
Kyle Carretto <kcarretto@gmail.com>
2020-01-08 18:12:35 -0800

    C2 Submit Task Results on agent checkin

--- pkg/c2/mocks/teamserver.gen.go
+++ pkg/c2/mocks/teamserver.gen.go
@@ -49,3 +49,17 @@ func (mr *MockTeamserverMockRecorder) ClaimTasks(arg0, arg1 interface{}) *gomock
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ClaimTasks", reflect.TypeOf((*MockTeamserver)(nil).ClaimTasks), arg0, arg1)
 }
+
+// SubmitTaskResult mocks base method
+func (m *MockTeamserver) SubmitTaskResult(arg0 context.Context, arg1 models.SubmitTaskResultRequest) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "SubmitTaskResult", arg0, arg1)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// SubmitTaskResult indicates an expected call of SubmitTaskResult
+func (mr *MockTeamserverMockRecorder) SubmitTaskResult(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SubmitTaskResult", reflect.TypeOf((*MockTeamserver)(nil).SubmitTaskResult), arg0, arg1)
+}
