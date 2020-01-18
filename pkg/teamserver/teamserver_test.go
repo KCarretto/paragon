285a024684ab23c2f607b2ae788bbc883fd8f7fc
koalatea <koalateac@gmail.com>
2020-01-17 23:41:31 -0600

    first test involving actual query

--- pkg/teamserver/teamserver_test.go
+++ pkg/teamserver/teamserver_test.go
@@ -1,7 +1,9 @@
 package teamserver_test
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -19,7 +21,6 @@ func TestTeamserver(t *testing.T) {
 		log.Fatalf("failed opening connection to sqlite: %v", err)
 	}
 	defer client.Close()
-	// run the auto migration tool.
 	if err := client.Schema.Create(context.Background()); err != nil {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
@@ -37,4 +38,30 @@ func TestTeamserver(t *testing.T) {
 			status, http.StatusUnprocessableEntity)
 	}
 
+}
+
+func TestTargetsReturnsCorrectly(t *testing.T) {
+	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer client.Close()
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+	srv := &teamserver.Server{EntClient: client}
+	router := teamserver.NewRouter(srv)
+
+	rr := httptest.NewRecorder()
+	query := "{\"query\": \"{ targets { id } }\"}"
+	req, err := http.NewRequest("POST", "/graphql", bytes.NewBufferString(query))
+	req.Header.Set("Content-Type", "application/json")
+
+	router.ServeHTTP(rr, req)
+
+	fmt.Printf("%s", rr.Body.String())
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusUnprocessableEntity)
+	}
 }
