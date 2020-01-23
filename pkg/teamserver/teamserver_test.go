832c89ef7324819aca6c6c2916f52385b967ab07
koalatea <koalateac@gmail.com>
2020-01-22 20:36:13 -0600

    first actual query test complete without assertion

--- pkg/teamserver/teamserver_test.go
+++ pkg/teamserver/teamserver_test.go
@@ -3,6 +3,7 @@ package teamserver_test
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -14,6 +15,12 @@ import (
 
 	_ "github.com/mattn/go-sqlite3"
 )
+
+// An Error returned by the GraphQL server
+type Error struct {
+	Message string   `json:"message"`
+	Path    []string `json:"path"`
+}
 
 func TestTeamserver(t *testing.T) {
 	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
@@ -49,6 +56,10 @@ func TestTargetsReturnsCorrectly(t *testing.T) {
 	if err := client.Schema.Create(context.Background()); err != nil {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
+	_, err = client.Target.Create().SetHostname("test").SetName("test").SetPrimaryIP("test").Save(context.Background())
+	if err != nil {
+		t.Errorf("failed to create target: %w", err)
+	}
 	srv := &teamserver.Server{EntClient: client}
 	router := teamserver.NewRouter(srv)
 
@@ -64,4 +75,16 @@ func TestTargetsReturnsCorrectly(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusUnprocessableEntity)
 	}
+	// Prepare response
+	var resp struct {
+		Data struct {
+			Targets []*ent.Target `json:"targets"`
+		} `json:"data"`
+		Errors []Error `json:"errors"`
+	}
+	data := json.NewDecoder(rr.Body)
+	if err := data.Decode(&resp); err != nil {
+		t.Errorf("failed to decode response: %w", err)
+	}
+	// assert a len 1 here
 }
