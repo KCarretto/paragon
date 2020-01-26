42076dead2cafdb97f199acf77c271d17e3790e9
Kyle Carretto <kcarretto@gmail.com>
2020-01-26 15:50:17 -0800

    Fetch current user before rendering layout

--- www/src/components/layout/XLayout.tsx
+++ www/src/components/layout/XLayout.tsx
@@ -1,24 +1,56 @@
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 import * as React from "react";
 import { FunctionComponent } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { RouteConfig } from "../../config/routes";
+import { User } from "../../graphql/models";
 import XLogin from "../../views/XLogin";
+import { XLoadingMessage } from "./components/messages";
 import XSidebar from "./XSidebar";
+
+const WHOAMI_QUERY = gql`
+  query WhoAmI {
+    me {
+      id
+    }
+  }
+`;
+
+type WhoAmIResult = {
+  data: User;
+};
 
 type LayoutProps = {
   routeMap: RouteConfig[];
   className: string;
 };
 
-const XLayout: FunctionComponent<LayoutProps> = props => (
-  <Router>
-    <Switch>
-      <Route path="/login" component={XLogin} />
-      <Route path="/">
-        <XSidebar routeMap={props.routeMap}>{props.children}</XSidebar>
-      </Route>
-    </Switch>
-  </Router>
-);
+const XLayout: FunctionComponent<LayoutProps> = props => {
+  const { loading } = useQuery<WhoAmIResult>(WHOAMI_QUERY, {
+    fetchPolicy: "no-cache"
+  });
+
+  if (loading) {
+    return (
+      <XLoadingMessage
+        title="Loading Paragon"
+        msg="Querying identity..."
+        hidden={false}
+      />
+    );
+  }
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login" component={XLogin} />
+        <Route path="/">
+          <XSidebar routeMap={props.routeMap}>{props.children}</XSidebar>
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default XLayout;
