9df7c92042faa47e465d46fd4e970ac9ddcf51b4
Kyle Carretto <kcarretto@gmail.com>
2020-01-28 22:30:25 -0800

    Bulk add credentials. Fixed target view

--- www/src/components/target/XTargetHeader.tsx
+++ www/src/components/target/XTargetHeader.tsx
@@ -1,3 +1,4 @@
+import moment from "moment";
 import * as React from "react";
 import { Header, Icon, IconProps } from "semantic-ui-react";
 import { Tag } from "../../graphql/models";
@@ -6,12 +7,18 @@ import { XTags } from "../tag";
 type TargetHeaderParams = {
   name: string;
   tags: Tag[];
+  lastSeen?: string;
   icon?: React.CElement<IconProps, Icon>;
 };
 
-export default ({ name, tags, icon }: TargetHeaderParams) => (
+export default ({ name, lastSeen, tags, icon }: TargetHeaderParams) => (
   <Header size="huge">
     {icon ? icon : <Icon name="desktop" />}
+    {!lastSeen || moment(lastSeen).isBefore(moment().subtract(5, "minutes")) ? (
+      <Icon name="times circle" color="red" size="large" />
+    ) : (
+      <Icon name="check circle" color="green" size="large" />
+    )}
     <Header.Content>{name}</Header.Content>
     <Header.Subheader>
       <XTags tags={tags} />
