2678bf4cdcde551bc70feee99e9bae265dc9136e
Kyle Carretto <kcarretto@gmail.com>
2020-01-31 12:17:34 -0800

    Disable CORS if env is set when build with dev mode. Added MySQL to docker compose

--- cmd/teamserver/server_dev.go
+++ cmd/teamserver/server_dev.go
@@ -3,18 +3,19 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"os"
 	"strings"
 )
 
 func serve(addr string, svc http.Handler) error {
+	if disableCORS := os.Getenv("PG_DISABLE_CORS"); disableCORS == "" {
+		return http.ListenAndServe(addr, svc)
+	}
+
 	withCORSDisabled := func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
-
-		fmt.Printf("REQUEST URI: %v\n")
-		fmt.Printf("URL PATH: %v\n", r.URL.Path)
 
 		if r.RequestURI == "/" || r.RequestURI == "/login" || strings.HasPrefix(r.RequestURI, "/app") {
 			http.Redirect(w, r, "http://127.0.0.1:8080"+r.URL.Path, http.StatusTemporaryRedirect)
