2857644b24c9c3e9398f0d0ee0dcef196f4ec023
koalatea <koalateac@gmail.com>
2020-02-03 20:12:16 -0600

    adding more tests

--- pkg/teamserver/teamserver_test.go
+++ pkg/teamserver/teamserver_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 
 	"github.com/kcarretto/paragon/ent"
+	"github.com/kcarretto/paragon/graphql/resolve"
 	"github.com/kcarretto/paragon/pkg/teamserver"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -23,19 +24,14 @@ type Error struct {
 }
 
 func TestTeamserver(t *testing.T) {
-	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
-	if err != nil {
-		log.Fatalf("failed opening connection to sqlite: %v", err)
-	}
+	client := testClient()
 	defer client.Close()
-	if err := client.Schema.Create(context.Background()); err != nil {
-		log.Fatalf("failed creating schema resources: %v", err)
-	}
+
 	srv := &teamserver.Server{EntClient: client}
 	router := teamserver.NewRouter(srv)
 
 	rr := httptest.NewRecorder()
-	req, err := http.NewRequest("GET", "/graphql", nil)
+	req, _ := http.NewRequest("GET", "/graphql", nil)
 
 	router.ServeHTTP(rr, req)
 
@@ -47,16 +43,21 @@ func TestTeamserver(t *testing.T) {
 
 }
 
-func TestTargetsReturnsCorrectly(t *testing.T) {
+func testClient() *ent.Client {
 	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
 	if err != nil {
 		log.Fatalf("failed opening connection to sqlite: %v", err)
 	}
-	defer client.Close()
 	if err := client.Schema.Create(context.Background()); err != nil {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
-	_, err = client.Target.Create().SetHostname("test").SetName("test").SetPrimaryIP("test").Save(context.Background())
+	return client
+}
+
+func TestTargetsReturnsCorrectly(t *testing.T) {
+	client := testClient()
+	defer client.Close()
+	_, err := client.Target.Create().SetHostname("test").SetName("test").SetPrimaryIP("test").Save(context.Background())
 	if err != nil {
 		t.Errorf("failed to create target: %w", err)
 	}
@@ -86,5 +87,77 @@ func TestTargetsReturnsCorrectly(t *testing.T) {
 	if err := data.Decode(&resp); err != nil {
 		t.Errorf("failed to decode response: %w", err)
 	}
-	// assert a len 1 here
+	if len(resp.Data.Targets) != 1 {
+		t.Errorf("Targets had more targets than were expected returned")
+	}
+}
+
+// test @master:/graphql/generated/generated.go -> MutationResolver and QueryResolver are the interfaces
+
+func TestTargetQuery(t *testing.T) {
+	client := testClient()
+	defer client.Close()
+	_, err := client.Target.Create().SetHostname("test").SetName("test").SetPrimaryIP("test").Save(context.Background())
+	_, err = client.Target.Create().SetHostname("test2").SetName("test2").SetPrimaryIP("test2").Save(context.Background())
+	if err != nil {
+		t.Errorf("failed to create target: %w", err)
+	}
+	resolver := &resolve.Resolver{EntClient: client}
+	query := resolver.Query()
+	targets, err := query.Targets(context.Background())
+	if err != nil {
+		t.Errorf("Targets query failed with %w", err)
+	}
+	if len(targets) != 2 {
+		t.Errorf("Targets query failed to get the targets \n expected: %d \n given: %d", 2, len(targets))
+	}
+	t1, t2 := false, false
+	for _, t := range targets {
+		if t.Name == "test" {
+			t1 = true
+		} else if t.Name == "test2" {
+			t2 = true
+		}
+	}
+	if !(t1 && t2) {
+		t.Errorf("Did not get the correct names for the targets")
+	}
+
+}
+
+func TestTargetsQuery(t *testing.T) {
+	client := testClient()
+	defer client.Close()
+	target, err := client.Target.Create().SetHostname("test").SetName("test").SetPrimaryIP("test").Save(context.Background())
+	if err != nil {
+		t.Errorf("failed to create target: %w", err)
+	}
+	resolver := &resolve.Resolver{EntClient: client}
+	query := resolver.Query()
+	queriedTarget, err := query.Target(context.Background(), target.ID)
+	if err != nil {
+		t.Errorf("Target query failed with %w", err)
+	}
+	if queriedTarget.ID != target.ID {
+		t.Errorf("Target query failed to get the correct target \n expected: %#v \n given: %#v", target, queriedTarget)
+	}
+
+}
+func TestWithoutHttp(t *testing.T) {
+	client := testClient()
+	defer client.Close()
+	target, err := client.Target.Create().SetHostname("test").SetName("test").SetPrimaryIP("test").Save(context.Background())
+	if err != nil {
+		t.Errorf("failed to create target: %w", err)
+	}
+	resolver := &resolve.Resolver{EntClient: client}
+	query := resolver.Query()
+	queriedTarget, err := query.Target(context.Background(), target.ID)
+	if err != nil {
+		t.Errorf("Target query failed with %w", err)
+	}
+	if queriedTarget.ID != target.ID {
+		t.Errorf("Target query failed to get the correct target \n expected: %#v \n given: %#v", target, queriedTarget)
+	}
+
 }
