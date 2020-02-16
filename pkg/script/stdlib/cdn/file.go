b3b750f69b5fd31bc2e5455f30ce0fac7dfd6876
Kyle Carretto <kcarretto@gmail.com>
2020-02-15 17:24:13 -0800

    Yep comments

--- pkg/script/stdlib/cdn/file.go
+++ pkg/script/stdlib/cdn/file.go
@@ -40,26 +40,32 @@ func (f *File) Read(p []byte) (int, error) {
 	return f.content.Read(p)
 }
 
+// Name provides the name that was provided to openFile.
 func (f *File) Name() string {
 	return path.Base(f.Path)
 }
 
+// Chmod does not work.
 func (f *File) Chmod(os.FileMode) error {
 	return fmt.Errorf("CDN does not support chmod")
 }
 
+// Chown does not work.
 func (f *File) Chown(uid, gid int) error {
 	return fmt.Errorf("CDN does not support chown")
 }
 
+// Stat does not work.
 func (f *File) Stat() (os.FileInfo, error) {
 	return nil, fmt.Errorf("CDN does not support stat")
 }
 
+// Move does not work.
 func (f *File) Move(dstPath string) error {
 	return fmt.Errorf("CDN does not support move")
 }
 
+// Remove does not work.
 func (f *File) Remove() error {
 	return fmt.Errorf("CDN does not support remove")
 }
