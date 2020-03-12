7b89d04cc3f31694fefeae61d0ce6faf827c2400
Kyle Carretto <kcarretto@gmail.com>
2020-03-11 21:42:18 -0700

    Improved & fixed operations test

--- pkg/script/stdlib/file/operations_test.go
+++ pkg/script/stdlib/file/operations_test.go
@@ -5,49 +5,129 @@ import (
 	"context"
 	"testing"
 
-	gomock "github.com/golang/mock/gomock"
 	"github.com/kcarretto/paragon/pkg/script"
+	"github.com/kcarretto/paragon/pkg/script/stdlib/assert"
 	"github.com/kcarretto/paragon/pkg/script/stdlib/file"
 	"github.com/kcarretto/paragon/pkg/script/stdlib/sys"
 	"github.com/stretchr/testify/require"
 )
 
-//go:generate mockgen -destination=operations.gen_test.go -package=file_test github.com/kcarretto/paragon/pkg/script/stdlib/file File
-
-func testLib(f file.File) script.Option {
-	return script.WithLibrary("testlib", script.Library(map[string]script.Func{
-		"f": script.Func(func(parser script.ArgParser) (script.Retval, error) {
-			return file.New(f), nil
-		})}),
-	)
-}
-
-func execScript(t *testing.T, f file.File, name string, code string) error {
-	return script.New(name, bytes.NewBufferString(code), testLib(f),
+func execScript(t *testing.T, name string, code string) error {
+	return script.New(name, bytes.NewBufferString(code),
+		assert.Include(),
 		file.Include(),
 		sys.Include(),
 	).
 		Exec(context.Background())
 }
 
-const codeTestMove = `
-def main():
-	f = sys.openFile("path/to/src")
-	f2 = testlib.f()
-	file.move(f, "dst")
-	file.move(f2, "dst")
+const codeTestOperations = `
+def test_sys_openFile(fileName):
+	f, err = sys.openFile(fileName)
+	assert.noError(err)
+	return f
 
+def test_sys_exec(cmd):
+	out, err = sys.exec(cmd)
+	assert.noError(err)
+	print("============= Exec =============")
+	print("Ran Command: "+cmd)
+	print(out)
+	print("============= ---- =============")
+
+def test_name(f):
+	print("============= Name =============")
+	print(file.name(f))
+	print("============= ---- =============")
+
+def test_write(f, fileContent):
+	path = file.name(f)
+
+	err = file.write(f, fileContent)
+	assert.noError(err)
+
+	f2 = test_sys_openFile(path)
+
+	content, err = file.content(f2)
+	assert.noError(err)
+
+	if content != fileContent:
+		fail("Failed to write expected content to file", fileContent, content)
+
+def test_content(f, fileContent):
+	print("============= Content =============")
+	content, err = file.content(f)
+	assert.noError(err)
+	print(content)
+	print("============= ---- =============")
+
+def test_move(f, dstPath):
+	srcPath = file.name(f)
+	err = file.move(f, dstPath)
+	assert.noError(err)
+
+	oldF = test_sys_openFile(srcPath)
+	content, err = file.content(f)
+	assert.noError(err)
+
+	oldContent, err = file.content(oldF)
+	assert.noError(err)
+
+def test_copy(f1, dstPath):
+	srcPath = file.name(f1)
+	content1, err = file.content(f1)
+	assert.noError(err)
+
+	f2 = test_sys_openFile(dstPath)
+	err = file.copy(f1, f2)
+	assert.noError(err)
+
+	content2, err = file.content(f2)
+	assert.noError(err)
+
+	if content1 != content2:
+		fail("Copied file content differed from original content", content1, content2)
+	return f2
+
+def test_remove(path):
+	f = test_sys_openFile(path)
+	err = file.remove(f)
+	assert.noError(err)
+
+def test_chown(f):
+
+	err = file.chown(f, "root", "root")
+	assert.noError(err)
+
+def test_chmod(f):
+	err = file.chmod(f, "123")
+	assert.noError(err)
+
+def main():
+	os = sys.detectOS()
+
+	prefix = "/tmp/paragon_test/" if os != "windows" else "C:\\Windows\\"
+	cmd = "ls -al /" if os != "windows" else "dir"
+
+	fileName = prefix + "path/to/file"
+	newPath = prefix + "new_path/to/file"
+	newNewPath = prefix + "new_new_path/to/file"
+	fileContent = "boop"
+
+	print("Opening File: "+fileName)
+	f = test_sys_openFile(fileName)
+	test_name(f)
+	test_write(f, fileContent)
+	test_content(f, fileContent)
+	test_move(f, newPath)
+	f = test_copy(f, newNewPath)
+	test_remove(newPath)
+	test_chown(f)
+	test_chmod(f)
+	test_sys_exec(cmd)
 `
 
-func TestMove(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	dst := "dst"
-
-	f := NewMockFile(ctrl)
-	f.EXPECT().Move(gomock.Eq(dst)).Return(nil)
-
-	err := execScript(t, f, "move_test", codeTestMove)
+func TestOperations(t *testing.T) {
+	err := execScript(t, "operations_test_script", codeTestOperations)
 	require.NoError(t, err, "script failed execution")
 }
