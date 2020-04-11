8dee1f39ddf33ad0888a4d720f81a8044d7e3104
Kyle Carretto <kcarretto@gmail.com>
2020-04-10 17:10:56 -0700

    Add Call method to script

--- pkg/script/script.go
+++ pkg/script/script.go
@@ -20,43 +20,63 @@ type Script struct {
 	ID        string
 	Builtins  map[string]Func
 	Libraries map[string]Library
+
+	thread  *starlark.Thread
+	symbols starlark.StringDict
 }
 
-// Exec parses input from the script reader and executes it. It will also invoke the EntryPoint
-// method if one is available.
-func (script Script) Exec(ctx context.Context) error {
-	builtins := script.compilePredeclared()
+// Exec calls the entrypoint method of a script
+func (script *Script) Exec(ctx context.Context) error {
+	_, err := script.Call(EntryPoint, starlark.Tuple{})
+	return err
+}
+
+// Call a function defined by the script.
+// This will execute all top level statements before calling the function.
+func (script *Script) Call(funcName string, args starlark.Tuple, kwargs ...starlark.Tuple) (starlark.Value, error) {
 	thread := script.newThread()
-
-	symbols, err := starlark.ExecFile(thread, script.ID, script.Reader, builtins)
+	symbols, err := script.parseSymbols(thread)
 	if err != nil {
-		if evalErr, ok := err.(*starlark.EvalError); ok {
-			return fmt.Errorf("%s", evalErr.Backtrace())
-		}
-		return err
+		return starlark.None, err
 	}
 
-	fn, ok := symbols[EntryPoint].(starlark.Callable)
+	fn, ok := symbols[funcName].(starlark.Callable)
 	if !ok {
-		return nil
+		return starlark.None, nil
 	}
 
-	res, err := starlark.Call(thread, fn, starlark.Tuple{}, []starlark.Tuple{})
+	res, err := starlark.Call(thread, fn, args, kwargs)
 	if err != nil {
 		if evalErr, ok := err.(*starlark.EvalError); ok {
-			return fmt.Errorf("%s", evalErr.Backtrace())
+			return starlark.None, fmt.Errorf("%s", evalErr.Backtrace())
 		}
-		return err
+		return starlark.None, err
 	}
 
 	if _, ok := res.(starlark.NoneType); !ok {
 		thread.Print(thread, res.String())
 	}
 
-	return nil
+	return res, nil
 }
 
-func (script Script) compilePredeclared() starlark.StringDict {
+func (script *Script) parseSymbols(thread *starlark.Thread) (starlark.StringDict, error) {
+	if script.symbols == nil {
+		builtins := script.compilePredeclared()
+		symbols, err := starlark.ExecFile(thread, script.ID, script.Reader, builtins)
+		if err != nil {
+			if evalErr, ok := err.(*starlark.EvalError); ok {
+				return nil, fmt.Errorf("%s", evalErr.Backtrace())
+			}
+			return nil, err
+		}
+		script.symbols = symbols
+	}
+
+	return script.symbols, nil
+}
+
+func (script *Script) compilePredeclared() starlark.StringDict {
 	builtins := make(starlark.StringDict)
 
 	for name, fn := range script.Builtins {
@@ -69,41 +89,45 @@ func (script Script) compilePredeclared() starlark.StringDict {
 	return builtins
 }
 
-func (script Script) newThread() *starlark.Thread {
-	name := "Renegade"
-	if script.ID != "" {
-		name = script.ID
-	}
-	return &starlark.Thread{
-		Name: name,
-		Print: func(_ *starlark.Thread, msg string) {
-			// TODO: Handle error
-			fmt.Fprintf(script.Writer, "%s\n", msg)
-		},
-		Load: func(_ *starlark.Thread, module string) (starlark.StringDict, error) {
-			lib, ok := script.Libraries[module]
-			if !ok || lib == nil {
-				// TODO: Wrap error with module name & script id.
-				return nil, ErrMissingLibrary
-			}
+func (script *Script) newThread() *starlark.Thread {
+	if script.thread == nil {
+		name := "Renegade"
+		if script.ID != "" {
+			name = script.ID
+		}
+		script.thread = &starlark.Thread{
+			Name: name,
+			Print: func(_ *starlark.Thread, msg string) {
+				// TODO: Handle error
+				fmt.Fprintf(script.Writer, "%s\n", msg)
+			},
+			Load: func(_ *starlark.Thread, module string) (starlark.StringDict, error) {
+				lib, ok := script.Libraries[module]
+				if !ok || lib == nil {
+					// TODO: Wrap error with module name & script id.
+					return nil, ErrMissingLibrary
+				}
 
-			return lib.stringDict(), nil
-		},
+				return lib.stringDict(), nil
+			},
+		}
 	}
+
+	return script.thread
 }
 
 // New initializes and returns a script with the provided contents.
-func New(id string, content io.Reader, options ...Option) Script {
-	script := Script{
-		content,
-		ioutil.Discard,
-		id,
-		map[string]Func{},
-		map[string]Library{},
+func New(id string, content io.Reader, options ...Option) *Script {
+	script := &Script{
+		Reader:    content,
+		Writer:    ioutil.Discard,
+		ID:        id,
+		Builtins:  map[string]Func{},
+		Libraries: map[string]Library{},
 	}
 
 	for _, opt := range options {
-		opt(&script)
+		opt(script)
 	}
 
 	return script
